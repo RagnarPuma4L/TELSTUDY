ce247e1e2370bfbb038cc55c963aac3c33b69948
Allison <eva0p72@gmail.com>
2025-06-15 15:46:56 -0400

    Update telmi2.py

--- src/telmi2.py
+++ src/telmi2.py
@@ -2,7 +2,7 @@ import flet as ft
 import requests
 
 # API de Groq
-API_KEY = "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"
+API_KEY = "[API_KEY]"  #NO SUBIR API_KEY AL REPOSITORIO PLISSS ;-;, PUEDEN REVOCAR EL USO.
 
 # Personalidad de Telmi
 conversacion = [
